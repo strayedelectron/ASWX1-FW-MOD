234e7330e7c5f4246d88a1d75a440ee1fe7c1944
Bodengriller <50927649+Bodengriller@users.noreply.github.com>
2020-03-05 22:17:56 +0100

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -34,19 +34,19 @@
  * Verbose version identifier which should contain a reference to the location
  * from where the binary was downloaded or the source code was compiled.
  */
-#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION " ASWX1-FW-MOD-v1.0"
+#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION "ASWX1-FW-MOD-v1.2"
 
 /**
  * The STRING_DISTRIBUTION_DATE represents when the binary file was built,
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-//#define STRING_DISTRIBUTION_DATE "2020-01-31"
+//#define STRING_DISTRIBUTION_DATE "2020-03-05"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
  */
-//#define MACHINE_NAME "3D Printer"
+//#define MACHINE_NAME "Sidewinder X1"
 
 /**
  * The SOURCE_CODE_URL is the location where users will find the Marlin Source
